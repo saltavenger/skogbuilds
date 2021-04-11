0797058544f719224594b8a7984ccaaee482c0d7
Ali <ali.areskog@buildium.com>
2021-04-11 15:12:59 -0400

    why am I committing the public folder

--- public/12-8c51e277672decd7f9a9.js
+++
@@ -1,3 +0,0 @@
-/*! For license information please see 12-8c51e277672decd7f9a9.js.LICENSE.txt */
-(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{Jgta:function(e,n,a){"use strict";a.r(n);var r=a("zIRd");a.d(n,"default",(function(){return r.a}));r.a.registerVersion("firebase","8.2.10","app")}}]);
-//# sourceMappingURL=12-8c51e277672decd7f9a9.js.map
